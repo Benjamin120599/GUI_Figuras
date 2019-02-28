76c06afee89efc4392f2f1190658713835976a2f
Benjamin Viramontes <benja120599@gmail.com>
2019-02-28 12:36:38 -0600

    Se añadió el metodo perimetroRombo

--- src/edu/tecjerez/topicos/figuras/dosDimensiones/Rombo.java
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/Rombo.java
@@ -7,7 +7,11 @@ public class Rombo {
 		return area;
 	}
 	
-	
+	double perimetroRombo(double D, double d) {
+		double L2 = Math.pow(D/2, 2) + Math.pow(d/2, 2);
+		double perimetro = 4 * Math.sqrt(L2);
+		return perimetro; 
+	}
 	
 
 }
