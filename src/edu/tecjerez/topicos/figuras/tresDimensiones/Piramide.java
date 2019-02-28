1c221fa0350d4f1395981f9a6c8747a40e4a6044
Benjamin Viramontes <benja120599@gmail.com>
2019-02-28 12:48:28 -0600

    Se añadió el metodo areaPiramide

--- src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
+++ src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
@@ -1,5 +1,16 @@
 package edu.tecjerez.topicos.figuras.tresDimensiones;
 
 public class Piramide {
+	
+	
+	public double areaPiramide(double perimetro, double altura, double base) {
+		double areaLateral = perimetro * altura;
+		double areaBase = base * 2;
+		double areaTotal = areaLateral + (2*areaBase);
+		return areaTotal;
+	}
+	
+	
+	
 
 }
