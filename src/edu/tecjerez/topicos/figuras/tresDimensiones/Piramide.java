2066f7cefc670ad9bb708af605d243a1f043263d
Benjamin Viramontes <benja120599@gmail.com>
2019-02-28 12:51:01 -0600

    Se añadió el metodo volumenPiramide

--- src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
+++ src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
@@ -3,11 +3,16 @@ package edu.tecjerez.topicos.figuras.tresDimensiones;
 public class Piramide {
 	
 	
-	public double areaPiramide(double perimetro, double altura, double base) {
-		double areaLateral = perimetro * altura;
+	public double areaPiramide(double perimetroBase, double altura, double base) {
+		double areaLateral = perimetroBase * altura;
 		double areaBase = base * 2;
 		double areaTotal = areaLateral + (2*areaBase);
 		return areaTotal;
+	}
+	
+	public double volumenPiramide(double base, double altura) {
+		double volumen = (1/3) * base * altura;
+		return volumen;		
 	}
 	
 	
